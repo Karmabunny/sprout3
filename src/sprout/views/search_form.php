0a54b20d91f09053cf88c0539d0d3b402fbe857d
LukeKB <luke@karmabunny.com.au>
2017-05-12 12:22:48 +0930

    Search form styling
    And removing old search form styles

--- src/sprout/views/search_form.php
+++ src/sprout/views/search_form.php
@@ -18,17 +18,21 @@ use Sprout\Helpers\Enc;
 ?>
 <form action="SITE/search" method="get">
 
-    <div class="field-element field-element--text">
-        <div class="field-label">
-            <label for="fm-site-search">Search the <?php echo Enc::html(Kohana::config('sprout.site_title')); ?> website</label>
+    <div class="field-element-attach-wrapper">
+        <div class="field-element field-element--text field-element--hidden-label">
+            <div class="field-label">
+                <label for="fm-site-search">Search the <?php echo Enc::html(Kohana::config('sprout.site_title')); ?> website</label>
+            </div>
+            <div class="field-input">
+                <input id="fm-site-search" class="textbox" type="text" name="q" value="<?php echo Enc::html(@$_GET['q']); ?>" placeholder="Enter your search here">
+            </div>
         </div>
-        <div class="field-input">
-            <input id="fm-site-search" class="textbox" type="text" name="q" value="<?php echo Enc::html(@$_GET['q']); ?>" placeholder="Enter your search here">
-        </div>
+        <button type="submit" class="field-element-attach-wrapper__button">
+            <span class="-vis-hidden">Search</span>
+            <svg class="icon icon-search"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-search"></use></svg>
+        </button>
     </div>
 
-    <div class="submit-bar">
-        <button type="submit" class="button">Search</button>
-    </div>
+
 
 </form>
