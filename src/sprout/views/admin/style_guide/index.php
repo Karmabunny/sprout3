9398dc8647090ec1536df6f43c89ea8ee8a06163
Josh Heidenreich <josh@karmabunny.com.au>
2017-05-17 07:50:20 +0930

    Style guide nav to be persistant across tabs

--- src/sprout/views/admin/style_guide/index.php
+++ src/sprout/views/admin/style_guide/index.php
@@ -1,7 +1,7 @@
-<p>
+<div class="white-box">
     <a href="admin/style_guide/general" class="button">General</a>
     <a href="admin/style_guide/forms" class="button">Forms</a>
     <a href="admin/style_guide/multiedit" class="button">Multiedit</a>
     <a href="admin/style_guide/richtext" class="button">Richtext</a>
     <a href="sprout/media/fonts/materialicons/demo.html" class="button">Icons</a>
-</p>
+</div>
