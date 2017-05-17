66b40e2e671a62d871ec2492090e4d574ef89ebd
Josh Heidenreich <josh@karmabunny.com.au>
2017-05-17 12:01:38 +0930

    Style guide demo of conditions list

--- src/sprout/views/admin/style_guide/index.php
+++ src/sprout/views/admin/style_guide/index.php
@@ -2,6 +2,7 @@
     <a href="admin/style_guide/general" class="button">General</a>
     <a href="admin/style_guide/forms" class="button">Forms</a>
     <a href="admin/style_guide/multiedit" class="button">Multiedit</a>
+    <a href="admin/style_guide/conditions_list" class="button">Conditions list</a>
     <a href="admin/style_guide/richtext" class="button">Richtext</a>
     <a href="sprout/media/fonts/materialicons/demo.html" class="button">Icons</a>
 </div>
