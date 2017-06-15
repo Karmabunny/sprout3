d1a619572c1b4af1f9bdfe31af07a6b625923356
Jana Hoffmann <jana@karmabunny.com.au>
2017-06-16 09:05:45 +0930

    Video Gallery front end styling

--- src/sprout/views/video_playlist.php
+++ src/sprout/views/video_playlist.php
@@ -9,14 +9,14 @@ Needs::fileGroup('video_gallery');
 ?>
 
 <?php if (!empty($videos) and count($videos) > 0): ?>
-<ul class="video-gallery-list -clearfix video-gallery-cols-<?php echo Enc::html($thumb_rows); ?>">
+<ul class="video-gallery -clearfix video-gallery--cols-<?php echo Enc::html($thumb_rows); ?>">
 <?php foreach ($videos as $video): ?>
-    <li class="video-gallery-list__item">
+    <li class="video-gallery__item">
         <a href="<?php echo Enc::html(Url::addUrlScheme('www.youtube.com/watch?v=' . $video['id'])); ?>">
             <img src="<?php echo Enc::html($video['thumb_url']); ?>" alt="<?php echo Enc::html($video['title']); ?>">
         </a>
         <?php if ($captions): ?>
-        <p class="video-gallery-list__item-caption"><?php echo Enc::html($video['title']); ?></p>
+        <p class="video-gallery__item-caption"><?php echo Enc::html($video['title']); ?></p>
         <?php endif; ?>
     </li>
 <?php endforeach; ?>
