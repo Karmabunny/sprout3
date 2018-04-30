859478bc095f6526b029b054e3f5e3e8c491e474
Christopher Aitken <chris@karmabunny.com.au>
2018-04-30 16:26:17 +0930

    Fix: update ChildrenGalleryWidget view with missing alt tag on images

--- src/sprout/views/children_page_gallery.php
+++ src/sprout/views/children_page_gallery.php
@@ -14,7 +14,7 @@ use Sprout\Helpers\File;
     ?>
 
     <li class="children-gallery-list-item children-gallery-item-mod<?php echo Enc::html($mod); ?> col-xs-6 col-sm-4 col-md-3">
-        <a href="<?php Enc::html($page->getFriendlyUrl()); ?>" class="children-gallery-list-item-link">
+        <a href="<?php echo Enc::html($page->getFriendlyUrl()); ?>" class="children-gallery-list-item-link">
             <?php if (!empty($page['gallery_thumb']) and File::exists($page['gallery_thumb'])): ?>
             <img src="<?php echo Enc::html(File::resizeUrl($page['gallery_thumb'], 'c300x260')); ?>" class="children-gallery-list-item-image" alt="" role="presentation">
             <?php else: ?>
