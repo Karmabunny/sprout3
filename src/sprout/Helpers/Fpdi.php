73100e9e46aa18ca63e1cd989863c97ef3393d4f
Gwilyn <gwilyn@karmabunny.com.au>
2021-02-18 10:34:28 +1030

    Swap in updated fpdf libraries.

--- src/sprout/Helpers/Fpdi.php
+++ src/sprout/Helpers/Fpdi.php
@@ -1,33 +1,14 @@
 <?php
-/*
- * kate: tab-width 4; indent-width 4; space-indent on; word-wrap off; word-wrap-column 120;
- * :tabSize=4:indentSize=4:noTabs=true:wrap=false:maxLineLen=120:mode=php:
- *
- * Copyright (C) 2016 Karmabunny Pty Ltd.
- *
- * This file is a part of SproutCMS.
- *
- * SproutCMS is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either
- * version 2 of the License, or (at your option) any later version.
- *
- * For more information, visit <http://getsproutcms.com>.
- */
-
 namespace Sprout\Helpers;
 
-use FPDI as UnderlyingFpdi;
-
-
-require 'fpdi/fpdi.php';
+use setasign\Fpdi\Fpdi as RealFdpi;
 
 /**
- * Implements FPDI library without having to hack up its source code (much).
- * FPDI is a PDF document importer which works with FPDF, so you can use nicely designed PDFs as templates,
- * and then dynamically add data to them.
- * See https://github.com/Setasign/FPDI or https://www.setasign.com/products/fpdi/about/
+ * DEPRECATED: Use `setassign\Fdpi\Fdpi`
+ *
+ * @deprecated Use `setassign\Fdpi\Fdpi`
+ * @package Sprout\Helpers
  */
-class Fpdi extends UnderlyingFpdi
+class Fpdi extends RealFdpi
 {
-    // See the FPDI class for the actual code
 }
