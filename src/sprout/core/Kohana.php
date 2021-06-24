5ffc480f37301b8323c968852a355e34af491f5e
Chris Aitken <chris@karmabunny.com.au>
2021-06-24 11:45:27 +0930

    Validate and populate sub-site code before use
    Edge case for unknown reasons a PROD site was attempting to load configs before sub-site was set

--- src/sprout/core/Kohana.php
+++ src/sprout/core/Kohana.php
@@ -957,10 +957,13 @@ final class Kohana {
         if ($directory === 'config')
         {
             array_unshift($paths, DOCROOT);
+
+            if (empty(SubsiteSelector::$subsite_code)) SubsiteSelector::selectSubsite();
             array_unshift($paths, DOCROOT . 'skin/' . SubsiteSelector::$subsite_code . '/');
         }
         else if ($directory === 'views')
         {
+            if (empty(SubsiteSelector::$subsite_code)) SubsiteSelector::selectSubsite();
             array_unshift($paths, DOCROOT . 'skin/' . SubsiteSelector::$subsite_code . '/');
         }
 
