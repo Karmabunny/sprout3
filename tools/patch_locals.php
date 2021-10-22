6124833b0d2712ac335f0734ab8105c7dced0558
Gwilyn <gwilyn@karmabunny.com.au>
2021-10-22 11:39:48 +1030

    Add locals patcher.

---
+++ tools/patch_locals.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+array_shift($argv);
+
+$dir = realpath(__DIR__ . '/../');
+chdir($dir);
+
+$vendor = realpath($dir . '/src/vendor/');
+if (!$vendor) die("Missing /src/vendor/\n");
+
+$composer = json_decode(file_get_contents($dir . '/composer.json'), true);
+$locals = $composer['extra']['locals'] ?: [];
+
+foreach ($locals as $package => $path) {
+    if (!file_exists($path)) {
+        echo "Target does not exist: {$path}\n";
+        continue;
+    }
+
+    $link = $vendor . '/' . trim($package, '/');
+    $target = '../../../' . trim($path, '/');
+
+    if (is_link($link)) {
+        exec("rm -f {$link}");
+    }
+    else if (file_exists($link)) {
+        exec("rm -rf {$link}");
+    }
+
+    symlink($target, $link);
+    echo "link: {$path} -> {$link}\n";
+}
+
+echo "\n";
+echo "Done!\n";
