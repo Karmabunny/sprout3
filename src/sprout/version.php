662c517077d0439ab11d2068e14a61c278eb71cc
chris <chris@karmabunny.com.au>
2022-02-02 15:46:45 +1030

    Add branding to sprout version file

--- src/sprout/version.php
+++ src/sprout/version.php
@@ -1,3 +1,13 @@
 <?php
 
-$config['version'] = 'trunk';
+$hash = '';
+
+if (file_exists(APPPATH . 'git_hash.txt')) {
+	ob_start();
+	require_once APPPATH . 'git_hash.txt';
+	$hash = ob_get_contents();
+	ob_end_clean();
+}
+
+$config['version_brand'] = 3.1;
+$config['version'] = sprintf('%s - #%s', $config['version_brand'], $hash);
