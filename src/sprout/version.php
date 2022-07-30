2ed9cffc2fe0fca28e73fa58dd64553fe458541c
Gwilyn <gwilyn@karmabunny.com.au>
2022-07-31 00:30:12 +0930

    Death to the git_hash.txt

--- src/sprout/version.php
+++ src/sprout/version.php
@@ -1,13 +1,14 @@
 <?php
 
-$hash = '';
+use Composer\InstalledVersions;
 
-if (file_exists(APPPATH . 'git_hash.txt')) {
-	ob_start();
-	require_once APPPATH . 'git_hash.txt';
-	$hash = ob_get_contents();
-	ob_end_clean();
+if (class_exists(InstalledVersions::class)) {
+        $version = InstalledVersions::getPrettyVersion('sproutcms/cms');
+        $hash = InstalledVersions::getReference('sproutcms/cms');
+} else {
+        $version = 'dev';
+        $hash = 'unknown';
 }
 
-$config['version_brand'] = 3.1;
-$config['version'] = sprintf('%s - #%s', $config['version_brand'], $hash);
+$config['version_brand'] = 3.2;
+$config['version'] = sprintf('%s - #%.7s', $version, $hash);
