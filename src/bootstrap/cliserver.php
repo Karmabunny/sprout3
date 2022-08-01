805604133b7ebc02e114cec13082379ac67d39ac
Gwilyn <gwilyn@karmabunny.com.au>
2022-08-01 13:53:28 +0930

    Add a CliServer helper - better rewrites, clearer intent.

--- src/bootstrap/cliserver.php
+++ src/bootstrap/cliserver.php
@@ -8,61 +8,12 @@
  * DO NOT USE THIS IN PRODUCTION.
  */
 
+use Sprout\Helpers\CliServer;
+
 if (PHP_SAPI !== 'cli-server') return;
 
-$url = parse_url($_SERVER['REQUEST_URI']);
-$_SERVER['QUERY_STRING'] = $url['query'] ?? '';
-$path = trim($url['path'], '/');
-
-// Serve it statically.
-if (is_file(DOCROOT . $path)) {
-    return false;
-}
-
-// Rewrites for media + skin files.
-// These are identical to those in .htaccess and nginx.conf.
-$count = 0;
-
-if (!$count) {
-    $target = preg_replace('!^media-[0-9]+/core/([a-z]+)/(.+)!', 'media/$1/$2', $path, -1, $count);
-}
-
-if (!$count) {
-    $target = preg_replace('!^media-[0-9]+/sprout/([a-z]+)/(.+)!', 'sprout/media/$1/$2', $path, -1, $count);
-}
-
-if (!$count) {
-    $target = preg_replace('!^media-[0-9]+/([_a-zA-Z0-9]+)/([a-z]+)/(.+)!', 'modules/$1/media/$2/$3', $path, -1, $count);
-}
-
-if (!$count) {
-    $target = preg_replace('!^skin-[0-9]+/([_a-z0-9\-]+)/([a-z]+)/(.+)!', 'skin/$1/$2/$3', $path, -1, $count);
-}
-
-$target = DOCROOT . $target;
-
-// Matched a rewrite.
-if ($count and file_exists($target)) {
-    $type = mime_content_type($target);
-
-    $matches = [];
-
-    // Built-in mime doesn't do js/css consistently.
-    if (preg_match('!\.(css)|(js)$!', $target, $matches)) {
-        if ($matches[1]) $type = 'text/css';
-        if ($matches[2]) $type = 'application/javascript';
-    }
-
-    // Fallback.
-    if (!$type) {
-        $type = 'text/plain';
-    }
-
-    header('Content-Type: ' . $type);
-    readfile($target);
-    return true;
-} else {
-    // Otherwise let sprout do it's thing.
+if (CliServer::serve()) {
     require __DIR__ . '/web.php';
-    return false;
 }
+
+return false;
---
+++ src/sprout/Helpers/CliServer.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Copyright (C) 2017 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+namespace Sprout\Helpers;
+
+/**
+ * Bridge the CLI-server environment with Kohana/Sprout.
+ *
+ * DO NOT USE THIS IN PRODUCTION.
+ *
+ * @package Sprout\Helpers
+ */
+class CliServer
+{
+
+    const REWRITES = [
+        '!^media-[0-9]+/(.+)!' => '_media/core/$1',
+        '!^sprout/media/(.+)!' => '_media/sprout/$1',
+        '!^modules/(.+)/media/(.+)!' => '_media/$1/$2',
+        '!^skin/(.+)!' => '_media/skin/$1',
+        '!^files/resize-([^-]+)-(.+)!' => 'files/resize/$1/$2',
+    ];
+
+
+    /**
+     * Perform rewrites.
+     *
+     * This should match those in the htaccess/nginx configurations.
+     *
+     * @param string $path
+     * @return null|string the rewritten path, or null if no match
+     */
+    public static function rewrites(string $path): ?string
+    {
+        foreach (self::REWRITES as $pattern => $replacement) {
+            $count = 0;
+            $path = preg_replace($pattern, $replacement, $path, 1, $count);
+
+            if ($count > 0) {
+                return $path;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Serve things.
+     *
+     * @return bool
+     *  - If true, continue the bootstrap (load kohana/sprout)
+     *  - otherwise false - do nothing and let the CLI-server handle the file request.
+     */
+    public static function serve(): bool
+    {
+        if (PHP_SAPI !== 'cli-server') {
+            die('CLI-server cannot be used outside of CLI-server mode.');
+        }
+
+        if (IN_PRODUCTION) {
+            die('What on Earth are you doing? No means no!');
+        }
+
+        // Kohana bootstrap hasn't happened yet, so Request/Router are
+        // empty at this point. Gotta parse it all ourselves.
+        $url = parse_url($_SERVER['REQUEST_URI']);
+        $_SERVER['QUERY_STRING'] = $url['query'] ?? '';
+        $path = trim($url['path'], '/');
+
+        // Perform rewrites.
+        $path = self::rewrites($path) ?? $path;
+
+        // Serve it statically.
+        if (is_file(DOCROOT . $path)) {
+            return false;
+        }
+
+        // Prep a URL for kohana.
+        // Like, if we did any rewrites.
+        $_GET['kohana_uri'] = $path;
+
+        return true;
+    }
+}
