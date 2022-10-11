eb0e7a1bf9d2eb986280271958f7786652a3f4a6
jamie monks <jamie.monks@gmail.com>
2022-10-11 15:54:14 +1030

    Migrating QrCode to Bacon QR + new methods

--- src/sprout/Helpers/QrCode.php
+++ src/sprout/Helpers/QrCode.php
@@ -1,8 +1,10 @@
 <?php
 namespace Sprout\Helpers;
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'phpqrcode/qrlib.php');
 
-use QRcode as QrCodeLib;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
 
 
 class QrCode
@@ -10,11 +12,58 @@ class QrCode
     /**
      * Render QR code as PNG
      *
-     * @param string $payload
+     * @param string $payload The content to encode in the QR code
+     * @param int $size The desired output size in pixels (square)
      * @return void Echos PNG directly
      */
-    public static function render($payload)
+    public static function render($payload, int $size = 500)
     {
-        QrCodeLib::png($payload, false, 'H', 16, 2);
+        $image = self::getString($payload, $size);
+
+        $size = strlen($image);
+        header("Content-type: image/png");
+        header("Content-length: $size");
+        echo $image;
+    }
+
+
+    /**
+     * Return QR code as PNG image string
+     *
+     * @param string $payload The content to encode in the QR code
+     * @param int $size The desired output size in pixels (square)
+     * @return string The PNG image as a string
+     */
+    public static function getString($payload, int $size = 500)
+    {
+        $renderer = new ImageRenderer(
+            new RendererStyle($size),
+            new ImagickImageBackEnd()
+        );
+        $writer = new Writer($renderer);
+
+        return $writer->writeString($payload);
+    }
+
+
+    /**
+     * Create a QR code and write to a specified file
+     *
+     * @param string $payload The content to encode in the QR code
+     * @param string $file_path The path and filename to write to
+     * @param int $size The desired output size in pixels (square)
+     * @return string The PNG image as a string
+     */
+    public static function create(string $payload, string $file_path, int $size = 500)
+    {
+        // Extract the extension type from the path. PNG is recommended
+        $ext = File::getExt($file_path);
+
+        $renderer = new ImageRenderer(
+            new RendererStyle($size),
+            new ImagickImageBackEnd($ext, 100)
+        );
+        $writer = new Writer($renderer);
+        $writer->writeFile($payload, $file_path);
     }
 }
