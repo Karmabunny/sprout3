50815f461ba05df259e251b1461054cfc97a8d54
Gwilyn <gwilyn@karmabunny.com.au>
2022-11-10 12:44:01 +1030

    Strip dead 'privDetermineWebDirectory' helper from phpunit bootstrap.

--- src/bootstrap/phpunit.php
+++ src/bootstrap/phpunit.php
@@ -35,33 +35,6 @@ chdir(DOCROOT);
 
 define('APPPATH', str_replace('\\', '/', realpath($kohana_application)).'/');
 
-/**
- * Used by the config bits, see the same function in index.php
- * @return string The root web path, e.g. '/' or '/my-subsite/'
- * @return bool False if the path couldn't be determined
- */
-function _privDetermineWebDirectory() {
-    if (!empty($_SERVER['PHP_S_WEBDIR'])) return $_SERVER['PHP_S_WEBDIR'];
-    if (! $_SERVER['DOCUMENT_ROOT']) return false;
-
-    $pos = strpos($_SERVER['SCRIPT_FILENAME'], $_SERVER['DOCUMENT_ROOT']);
-    if ($pos === 0) {
-        $doc_path = substr($_SERVER['SCRIPT_FILENAME'], strlen($_SERVER['DOCUMENT_ROOT']));
-        $doc_path = dirname($doc_path);
-
-        if (substr($doc_path, 0, 1) != '/') $doc_path = '/' . $doc_path;
-        if (substr($doc_path, -1, 1) != '/') $doc_path .= '/';
-
-        if ($_SERVER['REQUEST_URI'] and preg_match('!^/v[1-9]/!', $doc_path) and !preg_match('!^/v[1-9]/!', $_SERVER['REQUEST_URI'])) {
-            $doc_path = preg_replace('!^/v[1-9]/!', '/', $doc_path);
-        }
-
-        return $doc_path;
-    }
-
-    return false;
-}
-
 // Fake server vars when run from CLI
 if (empty($_SERVER['REMOTE_ADDR'])) $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
