4aa1e0cd774744df4d04b4c55221e4714c1875e2
Jamie Monks <jamie.monks@gmail.com>
2023-07-13 15:08:33 +0930

    Running file resizes from a local copy for speed
    Pulling the file from a remote every time is very slow

--- src/sprout/Helpers/File.php
+++ src/sprout/Helpers/File.php
@@ -1309,6 +1309,9 @@ class File
             $embed_text = false;
         }
 
+        // Get a local copy to avoid keep pulling remote images
+        $base_file = File::createLocalCopy($filename);
+
         foreach ($sizes as $size_name => $transform) {
             // Replicate the local temp file
             $temp_filename = STORAGE_PATH . 'temp/' . time() . '_' . str_replace('/', '~', $filename);
@@ -1377,6 +1380,8 @@ class File
 
             File::cleanupLocalCopy($temp_filename);
         }
+
+        File::cleanupLocalCopy($base_file);
     }
 
 
