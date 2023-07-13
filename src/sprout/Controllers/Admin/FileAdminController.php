3be05def332498200cdc3ec270e60c2e002ad366
Jamie Monks <jamie.monks@gmail.com>
2023-07-13 15:12:28 +0930

    Deleting all transforms during image replacement

--- src/sprout/Controllers/Admin/FileAdminController.php
+++ src/sprout/Controllers/Admin/FileAdminController.php
@@ -997,6 +997,7 @@ class FileAdminController extends HasCategoriesAdminController
 
         if ($file['type'] == FileConstants::TYPE_IMAGE and $needs_regenerate_sizes) {
             File::touch($file['filename']);
+            File::deleteTransforms($filename); // Any non-standard sizes will be invalid now
             File::createDefaultSizes($filename);
             File::deleteCache($filename);
         }
