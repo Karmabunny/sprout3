7f192021162407370f64812ddbd138bc36cd4984
Jamie Monks <jamie.monks@gmail.com>
2023-07-28 08:23:11 +0800

    Big simplification of files/s3 config

--- src/sprout/Helpers/Aws/S3.php
+++ src/sprout/Helpers/Aws/S3.php
@@ -3,6 +3,7 @@ namespace Sprout\Helpers\Aws;
 
 use Aws\Credentials\Credentials;
 use Aws\S3\S3Client;
+use Exception;
 use Kohana;
 
 /**
@@ -11,18 +12,20 @@ use Kohana;
 class S3
 {
 
-
-    public static function loadConfig(string $config_scope = 'default')
+    /**
+     * Load the AWS config from Kohana 'aws' config, with optional merge overrides
+     *
+     * @param array $config_merge
+     *
+     * @return array
+     */
+    public static function loadConfig(array $config_merge = [])
     {
         $config = Kohana::config('aws');
+        $config = array_merge($config, $config_merge);
 
         if (empty($config['region'])) {
-            throw new \Exception('AWS region not configured - is the .env file updated with AWS config?');
-        }
-
-        if ($config_scope != 'default') {
-            $config_merge = Kohana::config("aws.{$config_scope}");
-            $config = array_merge($config, $config_merge);
+            throw new Exception('AWS region not configured - is the .env file updated with AWS config?');
         }
 
         return $config;
@@ -34,10 +37,9 @@ class S3
      *
      * @return S3Client
      */
-    public static function getClient(string $config_scope = 'default')
+    public static function getClient(array $config_merge = [])
     {
-        $config = S3::loadConfig($config_scope);
-
+        $config = S3::loadConfig($config_merge);
         $credentials = new Credentials($config['credentials']['key'], $config['credentials']['secret']);
 
         $s3 = new S3Client(
@@ -49,65 +51,6 @@ class S3
         );
 
         return $s3;
-    }
-
-
-    /**
-     * Get the original files backend bucket URL for the current config set
-     */
-    public static function filesBackendBucketUrl()
-    {
-        $config = S3::loadConfig('files_backend');
-        return sprintf('https://%s.s3.%s.amazonaws.com/', $config['bucket'], $config['region']);
-    }
-
-
-    /**
-     * Get the path for a file in the original file bucket
-     *
-     * @param string $filename
-     *
-     * @return string
-     */
-    public static function filesBackendUrl(string $filename)
-    {
-        $base = S3::filesBackendBucketUrl();
-        return $base . $filename;
-    }
-
-
-    /**
-     * Get the file resize bucket URL for the current config set
-     */
-    public static function filesResizeBucketUrl()
-    {
-        $config = S3::loadConfig('files_resized');
-        return sprintf('https://%s.s3.%s.amazonaws.com/', $config['bucket'], $config['region']);
-    }
-
-
-    /**
-     * Get the path for a file in the resized file bucket
-     *
-     * @param string $filename
-     *
-     * @return string
-     */
-    public static function fileResizeUrl(string $filename)
-    {
-        $base = S3::filesResizeBucketUrl();
-        return $base . $filename;
-    }
-
-
-    /**
-     * Get a custom folder path based on the region and bucket in config
-     */
-    public static function filesBackendFolderUrl(string $folder_path)
-    {
-        $config = S3::loadConfig('files_backend');
-
-        return sprintf('https://%s.s3.%s.amazonaws.com/%s/', $config['bucket'], $config['region'], $folder_path);
     }
 
 }
