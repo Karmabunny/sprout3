c5aac629a2f01b7d3175d75d47c1d6735844a5b2
Jamie Monks <jamie.monks@gmail.com>
2023-07-30 17:03:25 +0800

    Better flle handling in file admin

--- src/sprout/Helpers/WorkerFileDefaultTransforms.php
+++ src/sprout/Helpers/WorkerFileDefaultTransforms.php
@@ -21,13 +21,28 @@ class WorkerFileDefaultTransforms extends WorkerBase
     /**
     * Do stuff
     **/
-    public function run(int $file_id)
+    public function run(int $file_id, bool $delete_existing = false)
     {
         ini_set('memory_limit', '1024M');
 
         Worker::message("Creating default transforms for file #{$file_id}");
+        Worker::message(' ');
 
-        File::createDefaultSizes($file_id, null, null);
+        if ($delete_existing) {
+            Worker::message('Deleting existing transforms...');
+
+            $res = FileTransform::deleteTransforms($file_id); // Any non-standard sizes will be invalid now
+            if (!$res) {
+                Worker::message('Failed to create image size versions - unable to delete old transforms');
+                Worker::failure();
+            }
+
+            Worker::message('Done.');
+            Worker::message(' ');
+        }
+
+        Worker::message('Creating default sizes...');
+        FileTransform::createDefaultTransforms($file_id);
 
         Worker::message('');
         Worker::success();
