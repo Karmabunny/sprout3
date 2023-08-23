90b36478722dec41f24d40bc4f817f16286e56b3
Gwilyn <gwilyn@karmabunny.com.au>
2023-08-23 15:21:26 +0930

    Update encryption helper to use openssl.
    This would be good to have tests.

--- src/sprout/Helpers/Encrypt.php
+++ src/sprout/Helpers/Encrypt.php
@@ -21,13 +21,10 @@ use Kohana_Exception;
 
 /**
  * The Encrypt library provides two-way encryption of text and binary strings
- * using the MCrypt extension ({@see http://php.net/mcrypt})
+ * using the openSSL extension.
  */
 class Encrypt
 {
-
-    // OS-dependant RAND type to use
-    protected static $rand;
 
     // Configuration
     protected $config;
@@ -56,46 +53,38 @@ class Encrypt
      */
     public function __construct($config = FALSE)
     {
-        if ( ! defined('MCRYPT_ENCRYPT'))
-            throw new Kohana_Exception('encrypt.requires_mcrypt');
-
         if (is_string($config))
-        {
-            $name = $config;
+		{
+			$name = $config;
 
-            // Test the config group name
-            if (($config = Kohana::config('encryption.'.$config)) === NULL)
-                throw new Kohana_Exception('encrypt.undefined_group', $name);
-        }
+			// Test the config group name
+			if (($config = Kohana::config('encryption.'.$config)) === NULL)
+				throw new Kohana_Exception('encrypt.undefined_group', $name);
+		}
 
-        if (is_array($config))
-        {
-            // Append the default configuration options
-            $config += Kohana::config('encryption.default');
-        }
-        else
-        {
-            // Load the default group
-            $config = Kohana::config('encryption.default');
-        }
+		if (is_array($config))
+		{
+			// Append the default configuration options
+			$config += Kohana::config('encryption.default');
+		}
+		else
+		{
+			// Load the default group
+			$config = Kohana::config('encryption.default');
+		}
 
-        if (empty($config['key']))
-            throw new Kohana_Exception('encrypt.no_encryption_key');
+		if (empty($config['key']))
+			throw new Kohana_Exception('encrypt.no_encryption_key');
 
-        // Find the max length of the key, based on cipher and mode
-        $size = mcrypt_get_key_size($config['cipher'], $config['mode']);
 
-        if (strlen($config['key']) > $size)
-        {
-            // Shorten the key to the maximum size
-            $config['key'] = substr($config['key'], 0, $size);
-        }
+		$ciphers = openssl_get_cipher_methods(true);
 
-        // Find the initialization vector size
-        $config['iv_size'] = mcrypt_get_iv_size($config['cipher'], $config['mode']);
+		if (!in_array($config['cipher'], $ciphers)) {
+			throw new Kohana_Exception('encrypt.invalid_cipher', $config['cipher']);
+		}
 
-        // Cache the config in the object
-        $this->config = $config;
+		// Cache the config in the object
+		$this->config = $config;
     }
 
     /**
@@ -106,49 +95,13 @@ class Encrypt
      */
     public function encode($data)
     {
-        // Set the rand type if it has not already been set
-        if (Encrypt::$rand === NULL)
-        {
-            if (DIRECTORY_SEPARATOR === '\\')
-            {
-                // Windows only supports the system random number generator
-                Encrypt::$rand = MCRYPT_RAND;
-            }
-            else
-            {
-                if (defined('MCRYPT_DEV_URANDOM'))
-                {
-                    // Use /dev/urandom
-                    Encrypt::$rand = MCRYPT_DEV_URANDOM;
-                }
-                elseif (defined('MCRYPT_DEV_RANDOM'))
-                {
-                    // Use /dev/random
-                    Encrypt::$rand = MCRYPT_DEV_RANDOM;
-                }
-                else
-                {
-                    // Use the system random number generator
-                    Encrypt::$rand = MCRYPT_RAND;
-                }
-            }
-        }
+        $iv = openssl_random_pseudo_bytes($this->config['iv_size']);
 
-        if (Encrypt::$rand === MCRYPT_RAND)
-        {
-            // The system random number generator must always be seeded each
-            // time it is used, or it will not produce true random results
-            mt_srand();
-        }
+		// Encrypt the data using the configured options and generated iv
+		$data = openssl_encrypt($data, $this->config['cipher'], $this->config['key'], 0, $iv);
 
-        // Create a random initialization vector of the proper size for the current cipher
-        $iv = mcrypt_create_iv($this->config['iv_size'], Encrypt::$rand);
-
-        // Encrypt the data using the configured options and generated iv
-        $data = mcrypt_encrypt($this->config['cipher'], $this->config['key'], $data, $this->config['mode'], $iv);
-
-        // Use base64 encoding to convert to a string
-        return base64_encode($iv.$data);
+		// Use base64 encoding to convert to a string
+		return base64_encode($iv.$data);
     }
 
     /**
@@ -160,16 +113,16 @@ class Encrypt
     public function decode($data)
     {
         // Convert the data back to binary
-        $data = base64_decode($data);
+		$data = base64_decode($data);
 
-        // Extract the initialization vector from the data
-        $iv = substr($data, 0, $this->config['iv_size']);
+		// Extract the initialization vector from the data
+		$iv = substr($data, 0, $this->config['iv_size']);
 
-        // Remove the iv from the data
-        $data = substr($data, $this->config['iv_size']);
+		// Remove the iv from the data
+		$data = substr($data, $this->config['iv_size']);
 
-        // Return the decrypted data, trimming the \0 padding bytes from the end of the data
-        return rtrim(mcrypt_decrypt($this->config['cipher'], $this->config['key'], $data, $this->config['mode'], $iv), "\0");
+		// Return the decrypted data, trimming the \0 padding bytes from the end of the data
+		return rtrim(openssl_decrypt($data, $this->config['cipher'], $this->config['key'], 0, $iv), "\0");
     }
 
 } // End Encrypt
--- src/sprout/config/encryption.php
+++ src/sprout/config/encryption.php
@@ -29,18 +29,10 @@
  *           long and contain letters, numbers, and symbols.
  *           @note Do not use a hash as your key. This significantly lowers encryption entropy.
  *
- *  mode   - MCrypt encryption mode. By default, MCRYPT_MODE_NOFB is used. This mode
- *           offers initialization vector support, is suited to short strings, and
- *           produces the shortest encrypted output.
- *           @see http://php.net/mcrypt
- *
- *  cipher - MCrypt encryption cipher. By default, the MCRYPT_RIJNDAEL_128 cipher is used.
- *           This is also known as 128-bit AES.
- *           @see http://php.net/mcrypt
+ *  cipher - openSSL encryption cipher. By default, the 'aes-256-cbc' cipher is used.
  */
 $config['default'] = array
 (
     'key'    => 'K0H@NA+PHP_7hE-SW!FtFraM3w0R|<',
-    'mode'   => MCRYPT_MODE_NOFB,
-    'cipher' => MCRYPT_RIJNDAEL_128
+    'cipher' => 'aes-256-cbc',
 );
