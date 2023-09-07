bce3bcd2bbe337f7bfcf14df7053949f617fab01
Jamie Monks <jamie.monks@gmail.com>
2023-09-07 10:18:32 +0800

    List views for email reports

---
+++ src/sprout/views/admin/generic_email_report_list.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * kate: tab-width 4; indent-width 4; space-indent on; word-wrap off; word-wrap-column 120;
+ * :tabSize=4:indentSize=4:noTabs=true:wrap=false:maxLineLen=120:mode=php:
+ *
+ * Copyright (C) 2015 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+use Sprout\Helpers\Enc;
+
+echo $itemlist;
+?>
+
+<p><a href="<?php echo Enc::html($add_url); ?>" class="button">Create new report</a></p>
