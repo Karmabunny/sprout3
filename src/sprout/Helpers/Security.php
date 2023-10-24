81177ac3c75319c241534722b6d66152e8c3b3f7
Jamie Monks <jamie.monks@gmail.com>
2023-10-24 13:46:10 +0800

    Honeypot set and check/die tooling in Security helper
    Honeypot field name can optionally be overriden
    by a config honeypot.field_name if desired

--- src/sprout/Helpers/Security.php
+++ src/sprout/Helpers/Security.php
@@ -17,6 +17,7 @@ use Exception;
 use InvalidArgumentException;
 use karmabunny\kb\Secrets;
 use Kohana;
+use Kohana_404_Exception;
 use Kohana_Exception;
 use Sprout\Exceptions\SignatureInvalidException;
 
@@ -26,6 +27,12 @@ use Sprout\Exceptions\SignatureInvalidException;
  */
 class Security
 {
+
+    /**
+     * A non-clashy name for the honeypot field
+     */
+    private static $honeypot_field = 'terms_special_set_143';
+
 
     /**
      *
@@ -258,6 +265,81 @@ class Security
         }
 
         return $errs;
+    }
+
+
+    /**
+     * Get the field name for a honeypot trap. Optionally override via config
+     *
+     * Add a config called 'honeypot.php' with an index value for 'field_name' to override the default
+     *
+     * @return string
+     */
+    private static function honeypotFieldName(): string
+    {
+        try {
+            return Kohana::config('honeypot.field_name');
+        } catch (Exception $e) {
+            return self::$honeypot_field;
+        }
+    }
+
+
+    /**
+     * Drop a honeypot form field
+     */
+    public static function honeypotSet()
+    {
+        $field_name = self::honeypotFieldName();
+        echo sprintf('<input type="text" name="%s" class="-vis-hidden" tabindex="-1" autocomplete="false">', $field_name);
+    }
+
+
+    /**
+     * See if anything stuck to the honey
+     *
+     * @param string $method Optional form method (POST|GET). Default of POST
+     *
+     * @return bool Validation flag - false if honeypot was triggered
+     */
+    public static function honeypotCheck($method = 'POST')
+    {
+        if (empty($method) or !in_array(strtoupper($method), array('POST', 'GET'))) $method = 'POST';
+
+        switch ($method)
+        {
+            case 'POST':
+                $value = $_POST[self::honeypotFieldName()] ?? null;
+                break;
+
+            case 'GET':
+                $value = $_GET[self::honeypotFieldName()] ?? null;
+                break;
+        }
+
+        if (!empty($value)) {
+            return false;
+        }
+
+
+        return true;
+    }
+
+
+    /**
+     * See if anything stuck to the honey. Explode if it did
+     *
+     * @param string $method Optional form method (POST|GET). Default of POST
+     *
+     * @return void Throws error
+     */
+    public static function honeypotCheckOrDie($method = 'POST')
+    {
+        $passed = self::honeypotCheck($method);
+
+        if (!$passed) {
+            throw new Kohana_404_Exception();
+        }
     }
 
 }
