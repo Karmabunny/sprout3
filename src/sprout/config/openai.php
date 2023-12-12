ebd0c80ca7e5911fd96ebfceac82fffd95e9fd1c
Jamie Monks <jamie.monks@gmail.com>
2023-12-12 08:35:57 +0800

    Initial max tokens config to 3000

--- src/sprout/config/openai.php
+++ src/sprout/config/openai.php
@@ -4,3 +4,11 @@
  * API Key for using GPT endpoints and such from OpenAI
  */
 $config['secret_key'] = '';
+
+
+/**
+ * Config options for chat completion requests
+ */
+$config['chat_completion'] = [
+    'max-tokens' => 3000,
+];
