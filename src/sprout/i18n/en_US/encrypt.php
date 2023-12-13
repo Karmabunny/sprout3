19387c35e5ecab5dd718c8b8bb867aeb598ba2fb
Gwilyn <gwilyn@karmabunny.com.au>
2023-12-13 10:41:09 +1030

    Remove last mcrypt bits.

--- src/sprout/i18n/en_US/encrypt.php
+++ src/sprout/i18n/en_US/encrypt.php
@@ -15,6 +15,5 @@
 $lang = array
 (
     'undefined_group'   => 'The %s group is not defined in your configuration.',
-    'requires_mcrypt'   => 'To use the Encrypt library, mcrypt must be enabled in your PHP installation',
     'no_encryption_key' => 'To use the Encrypt library, you must set an encryption key in your config file'
 );
