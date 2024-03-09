cedcabd91f21bd3800defd8332564606110183bd
Gwilyn <gwilyn@karmabunny.com.au>
2024-03-09 23:55:07 +1100

    Add module class templates.

---
+++ src/sprout/module_template/has_categories/MODULEModule.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (C) 2017 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+namespace SproutModules\AUTHOR\MODULE;
+
+use Sprout\Helpers\Module;
+use Sprout\Helpers\ModuleSiteTrait;
+
+/**
+ * MODULE module.
+ */
+class MODULEModule extends Module
+{
+    use ModuleSiteTrait;
+}
+
