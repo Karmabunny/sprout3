b95141f46ca0bff3daa46daf2617aab55c931f76
Gwilyn <gwilyn@karmabunny.com.au>
2024-11-18 19:03:43 +1100

    Fix multipleFileSelect() broken filenames query.

--- src/sprout/Helpers/Fb.php
+++ src/sprout/Helpers/Fb.php
@@ -1854,7 +1854,7 @@ class Fb
         if (count($ids) > 0) {
             $params = [];
             $where = Pdb::buildClause([['id', 'IN', $ids]], $params);
-            $filenames = Pdb::q("SELECT id, filename FROM ~files", $params, 'map');
+            $filenames = Pdb::q("SELECT id, filename FROM ~files WHERE {$where}", $params, 'map');
         }
 
         if (substr($name, -2) != '[]') $name .= '[]';
