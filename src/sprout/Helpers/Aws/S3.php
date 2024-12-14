74b3dbc97c7b7157c9667ce9516b3ec230f7b0a2
Jamie Monks <jamie.monks@gmail.com>
2024-12-14 12:59:11 +1030

    Updated AWS library helpers

--- src/sprout/Helpers/Aws/S3.php
+++ src/sprout/Helpers/Aws/S3.php
@@ -1,56 +1,29 @@
 <?php
 namespace Sprout\Helpers\Aws;
 
-use Aws\Credentials\Credentials;
 use Aws\S3\S3Client;
-use Exception;
 use Kohana;
 
 /**
  * A simple wrapper for loading S3 ready for use with the AWS SDK
  */
-class S3
+class S3 extends Aws
 {
-
-    /**
-     * Load the AWS config from Kohana 'aws' config, with optional merge overrides
-     *
-     * @param array $config_merge
-     *
-     * @return array
-     */
-    public static function loadConfig(array $config_merge = [])
-    {
-        $config = Kohana::config('aws');
-        $config = array_merge($config, $config_merge);
-
-        if (empty($config['region'])) {
-            throw new Exception('AWS region not configured - is the .env file updated with AWS config?');
-        }
-
-        return $config;
-    }
-
 
     /**
      * Get an S3Client instance with local configuration
      *
      * @return S3Client
      */
-    public static function getClient(array $config_merge = [])
+    public static function getClient(array $config = []): S3Client
     {
-        $config = S3::loadConfig($config_merge);
-        $credentials = new Credentials($config['credentials']['key'], $config['credentials']['secret']);
+        $sdk = self::getSdk();
+        $config['version'] = $config['version'] ?? '2006-03-01';
+        $config['region'] = $config['region'] ?? Kohana::config('aws.region');
 
-        $s3 = new S3Client(
-            [
-                'region' => $config['region'],
-                'version' => 'latest',
-                'credentials' => $credentials,
-            ]
-        );
+        $client = $sdk->createS3($config);
 
-        return $s3;
+        return $client;
     }
 
 }
