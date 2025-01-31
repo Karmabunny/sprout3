787e0877e56d43437326c5a8e209e3ea90b0db00
Gwilyn <gwilyn@karmabunny.com.au>
2025-02-01 00:05:17 +1100

    Add model rule tests.

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use karmabunny\kb\Uuid;
+use karmabunny\kb\ValidationException;
 use karmabunny\pdb\Exceptions\ConnectionException;
 use karmabunny\pdb\Models\PdbRawCondition;
 use karmabunny\pdb\PdbParser;
@@ -44,10 +45,12 @@ class ModelTest extends TestCase
         // A new model.
         $model = new ModelItem();
         $model->name = 'sprout test';
+        $model->status = 'pending';
 
         $expected = [
             'date_added',
             'name',
+            'status',
             'uid',
         ];
 
@@ -61,7 +64,7 @@ class ModelTest extends TestCase
         $this->assertNull($model->date_added);
 
         // Pdo is opened here.
-        $this->assertTrue($model->save());
+        $this->assertTrue($model->save(false));
         $this->assertGreaterThan(0, $model->id);
 
         sleep(1);
@@ -77,7 +80,7 @@ class ModelTest extends TestCase
         usleep(500 * 1000);
 
         $model->name = 'something else';
-        $this->assertTrue($model->save());
+        $this->assertTrue($model->save(false));
 
         // Some things change, others stay the same.
         $this->assertEquals($added, $model->date_added);
@@ -91,5 +94,62 @@ class ModelTest extends TestCase
         $this->assertEquals($model->uid, $other->uid);
         $this->assertEquals($model->date_added, $other->date_added);
         $this->assertEquals($model->name, $other->name);
+    }
+
+
+
+    public function testModelRules()
+    {
+        // A new model.
+        $model = new ModelItem();
+        $model->name = 'sprout test';
+        $model->status = 'pending';
+
+        $ok = $model->save();
+        $this->assertTrue($ok);
+
+        // Mess it up a bit.
+        $model->id = 0;
+        $model->name = '';
+        $model->status = 'lol what';
+        $errors = $model->valid();
+
+        $this->assertNotTrue($errors);
+        $this->assertArrayHasKey('uid', $errors);
+        $this->assertStringContainsStringIgnoringCase('unique', $errors['uid'][0]);
+
+        $this->assertArrayHasKey('name', $errors);
+        $this->assertArrayHasKey('required', $errors['name']);
+
+        $this->assertArrayHasKey('status', $errors);
+        $this->assertStringContainsStringIgnoringCase('invalid', $errors['status'][0]);
+
+        // Still not OK, but we're past the required rule.
+        $model->name = '1234';
+        $errors = $model->valid();
+
+        $this->assertArrayHasKey('name', $errors);
+        $this->assertArrayNotHasKey('required', $errors['name']);
+        $this->assertStringContainsStringIgnoringCase('shorter', $errors['name'][0]);
+
+        // One more for luck.
+        $other = new ModelItem();
+        $other->name = 'sprout test';
+        $other->status = 'ready';
+
+        try {
+            $other->save();
+            $this->fail('Expected validation error');
+
+        } catch (ValidationException $exception) {
+            $errors = $exception->getErrors();
+            $this->assertArrayHasKey('name', $errors);
+            $this->assertStringContainsStringIgnoringCase('unique', $errors['name'][0]);
+            $this->assertStringContainsStringIgnoringCase('name', $exception->getMessage());
+        }
+
+        // Fix it.
+        $other->name = 'other test';
+        $other->save();
     }
 }
