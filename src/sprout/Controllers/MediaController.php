bf131aca5c26ddd3c0078849ac88275bac23ad5e
Gwilyn <gwilyn@karmabunny.com.au>
2025-02-01 02:18:03 +1100

    Add media generate() route to mirror checksum paths.

--- src/sprout/Controllers/MediaController.php
+++ src/sprout/Controllers/MediaController.php
@@ -13,69 +13,54 @@
 
 namespace Sprout\Controllers;
 
-use BootstrapConfig;
-use Exception;
 use Kohana;
 use Kohana_404_Exception;
-use Kohana_Exception;
+use Sprout\Exceptions\MediaException;
 use Sprout\Helpers\AdminAuth;
 use Sprout\Helpers\File;
-use Sprout\Helpers\Modules;
 use Sprout\Helpers\Media;
-use Sprout\Helpers\Router;
+use Sprout\Helpers\Modules;
+use Sprout\Helpers\Subsites;
+use Sprout\Helpers\SubsiteSelector;
+use Sprout\Helpers\Url;
 use Throwable;
 
 /**
  * Serving media assets.
  *
  * These are not files, {@see FileController}. These are JS/CSS assets
- * typically loaded by the {@see Needs} helper.
+ * typically loaded by the {@see Needs} and {@see Media} helpers.
  */
 class MediaController extends Controller
 {
 
     /**
-     * Serve the file immediately.
+     * Serve
      *
-     * @param string $resource
-     * @param string $segments
-     * @return never
-     * @throws Kohana_404_Exception
+     * This exists to serve media files not already processed by the
+     * {@see Media} helper, such as relative urls.
+     *
+     * `_media/checksum/section/file`
+     *
+     * @return never serve the file
      */
-    public function serve(...$segments)
+    public function generate($section, ...$segments)
     {
-        $resource = array_shift($segments);
-        $url = implode('/', $segments);
+        // Resolve the root path.
+        $root = Media::getRoot($section, false);
+        $path = $root . implode('/', $segments);
 
-        if ($resource === 'core') {
-            $root = COREPATH . 'media/';
-
-        } elseif ($resource === 'sprout') {
-            $root = APPPATH . 'media/';
-
-        } elseif ($resource === 'skin') {
-            $root = DOCROOT . 'skin/';
-
-        } else if ($module = Modules::getModule($resource)) {
-            $root = $module->getPath() . 'media/';
-        }
-        else {
-            throw new Kohana_404_Exception($url);
-        }
-
-        $path = $root . $url;
-
-        if (!is_file($path)) {
-            throw new Kohana_404_Exception($url);
+        if (!file_exists($path)) {
+            throw new Kohana_404_Exception();
         }
 
         // Shush. Drop any existing output.
         Kohana::closeBuffers(false);
+        set_exception_handler(null);
 
         // Caching for 7 days.
         header('Cache-Control: cache, store, max-age=604800, must-revalidate');
 
-        // File types.
         $mimetype = File::mimetypeExtended($path);
         if ($mimetype) {
             header("Content-Type: {$mimetype}");
@@ -88,41 +73,43 @@ class MediaController extends Controller
         $ok = readfile($path);
 
         if ($ok === false) {
-            throw new Exception('Failed to read file: ' . $url);
+            throw new MediaException('Failed to read file: ' . $path);
         }
 
-        // Ok, really shush now.
-        // We don't want errors bleeding into the asset bodies.
-        set_exception_handler(null);
-        ini_set('display_errors', '0');
+        // Do the dirty.
+        try {
+            Media::generateUrl($path);
+        } catch (Throwable $ex) {
+            Kohana::logException($ex, true);
+        }
+    }
 
-        // Now copy it so this file doesn't hit the app again. This effectively
-        // 'shadows' the file in the same path as this controller. The
-        // web server (nginx, apache) should find and serve it before deferring
-        // to the PHP app.
-        // First check defined() so we don't break migrations.
-        if (defined('BootstrapConfig::ENABLE_MEDIA_CACHE') and constant('BootstrapConfig::ENABLE_MEDIA_CACHE')) {
-            try {
-                $dest = WEBROOT . Router::$current_uri;
-                $dir = dirname($dest);
 
-                // if (exists) mkdir() is not atomic. Another thread can always
-                // beat us to it. Do and ask for forgiveness later.
-                @mkdir($dir, 0777, true);
+    /**
+     *
+     *  `_media/section/path/to/file`
+     *
+     * @return never redirect to generated checksum URL
+     */
+    public function resolve(...$segments)
+    {
+        try {
+            $section = array_shift($segments);
+            $root = Media::getRoot($section, false);
 
-                if (!is_dir($dir)) {
-                    throw new Exception("Target directory is missing: {$dir}");
-                }
-
-                $ok = copy($path, $dest);
-                if (!$ok) throw new Exception("Failed to copy file: {$path} to {$dest}");
-            }
-            catch (Throwable $ex) {
-                Kohana::logException($ex, true);
-            }
+        } catch (MediaException $ex) {
+            array_unshift($segments, $section);
+            $root = Media::getRoot('core');
         }
 
-        exit;
+        try {
+            $path = $root . implode('/', $segments);
+            $url = Media::generateUrl($path);
+            Url::redirect($url);
+
+        } catch (MediaException $ex) {
+            throw new Kohana_404_Exception();
+        }
     }
 
 
--- src/sprout/config/routes.php
+++ src/sprout/config/routes.php
@@ -114,7 +114,8 @@ $config['file/play_audio/([^/]+)'] = 'Sprout\\Controllers\\FileController/playAu
 $config['file/download/([0-9]+)(?:/([a-z_]+))?'] = 'Sprout\\Controllers\\FileController/download/$1/$2';
 $config['file/name_lookup'] = 'Sprout\\Controllers\\FileController/nameLookup';
 
-$config['_media/(.+)'] = 'Sprout\\Controllers\\MediaController/serve/$1';
+$config['_media/(?:[a-f0-9]{8})/([^/]+)/(.+)'] = 'Sprout\\Controllers\\MediaController/generate/$1/$2';
+$config['_media/(.+)'] = 'Sprout\\Controllers\\MediaController/resolve/$1';
 $config['media_tools/(.+)'] = 'Sprout\\Controllers\\MediaController/$1';
 
 $config['tinymce4/image'] = 'Tinymce4Controller/image';
