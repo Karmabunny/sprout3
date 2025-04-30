c294f30c80c410cd870dc230ce1908244dd409e2
Gwilyn <gwilyn@karmabunny.com.au>
2025-04-30 10:17:12 +1000

    Reset the admin session when processing new logins.

--- src/sprout/Controllers/AdminController.php
+++ src/sprout/Controllers/AdminController.php
@@ -184,6 +184,7 @@ class AdminController extends Controller
 
         Session::instance();
         Session::regenerate();
+        unset($_SESSION['admin']);
 
         $_POST['Username'] = trim($_POST['Username']);
         $_POST['Password'] = trim($_POST['Password']);
