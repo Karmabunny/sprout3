8a72720f5b597aade9ed207154b55db9336d5ff4
Gwilyn <gwilyn@karmabunny.com.au>
2026-01-28 10:49:01 +1100

    Add worker interface.

---
+++ src/sprout/Helpers/WorkerInterface.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright (C) 2026 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+namespace Sprout\Helpers;
+
+/**
+ * Interface for worker jobs.
+ */
+interface WorkerInterface
+{
+
+    /**
+     * Gets the job name
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+
+    /**
+     * Gets the metric names.
+     *
+     * @return array<int,string>
+     **/
+    public function getMetricNames(): array;
+
+}
