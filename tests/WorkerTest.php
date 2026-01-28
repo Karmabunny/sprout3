35904b27b9b73ae57bc17c69d1b67c8d3789b353
Gwilyn <gwilyn@karmabunny.com.au>
2026-01-28 11:59:17 +1100

    Add test case for worker jobs.

--- tests/WorkerTest.php
+++ tests/WorkerTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use Sprout\Helpers\Pdb;
 use Sprout\Helpers\WorkerCtrl;
+use Sprout\TestModules\TestModule\Jobs\TestJob;
 use Sprout\TestModules\TestModule\Jobs\TestWorker;
 
 /**
@@ -26,5 +27,18 @@ class WorkerTest extends TestCase
         $this->assertEquals('Success', $job['status']);
     }
 
+
+    public function testJob()
+    {
+        $job = WorkerCtrl::start(TestJob::class, arg1: 5, arg2: 10, arg3: 15);
+
+        sleep(1);
+
+        $job = Pdb::get('worker_jobs', $job['job_id']);
+
+        $this->assertEquals(5, $job['metric1val']);
+        $this->assertEquals(10, $job['metric2val']);
+        $this->assertEquals(15, $job['metric3val']);
+    }
 
 }
---
+++ tests/modules/TestModule/Jobs/TestJob.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Copyright (C) 2026 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+namespace Sprout\TestModules\TestModule\Jobs;
+
+use Sprout\Helpers\Sprout;
+use Sprout\Helpers\Worker;
+use Sprout\Helpers\WorkerJob;
+
+/**
+ * An updated worker job, with support for channels.
+ */
+class TestJob extends WorkerJob
+{
+
+    public $arg1;
+
+    public $arg2;
+
+    public $arg3;
+
+
+    /** @inheritdoc */
+    public function getMetricNames(): array
+    {
+        return [
+            1 => 'First run',
+            2 => 'Second run',
+            3 => 'Final run',
+        ];
+    }
+
+
+    /** @inheritdoc */
+    public function run()
+    {
+        $this->log("It's running! ({$this->arg1})");
+
+        for ($i = 1; $i <= $this->arg1; ++$i) {
+            usleep(10000);
+            Worker::metric(1, $i);
+        }
+
+        $this->log("Still running! ({$this->arg2})");
+        $this->log('$_SERVER:');
+        $this->log(print_r($_SERVER, true));
+
+        for ($i = 1; $i <= $this->arg2; ++$i) {
+            usleep(10000);
+            Worker::metric(2, $i);
+        }
+
+        $this->log("Almost there! ({$this->arg3})");
+        $this->log('Sprout::absRoot():');
+        $this->log(Sprout::absRoot());
+
+        for ($i = 1; $i <= $this->arg3; ++$i) {
+            usleep(10000);
+            Worker::metric(3, $i);
+        }
+
+        $this->log('Finished');
+    }
+
+}
