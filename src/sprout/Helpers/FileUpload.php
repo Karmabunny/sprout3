c321c5b1cfe37d1feb7712a991a3c8ecd716ce69
Gwilyn <gwilyn@karmabunny.com.au>
2026-02-18 13:29:41 +1100

    Fix file upload ini errors with shorthand byte parser.

--- src/sprout/Helpers/FileUpload.php
+++ src/sprout/Helpers/FileUpload.php
@@ -220,7 +220,8 @@ class FileUpload
 
         if ($error == UPLOAD_ERR_INI_SIZE) {
             $max_filesize = ini_get('upload_max_filesize');
-            $max_filesize = File::humanSize((int) $max_filesize);
+            $max_filesize = Sprout::parseByteSize($max_filesize);
+            $max_filesize = File::humanSize($max_filesize);
             $message = str_replace('upload_max_filesize', $max_filesize, $message);
         }
         else if ($error == UPLOAD_ERR_FORM_SIZE) {
