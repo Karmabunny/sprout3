72d2abf7fc0c249f3c24fb1365faa57ee7188ee0
Gwilyn <gwilyn@karmabunny.com.au>
2026-02-25 09:19:35 +1100

    Make the app class configurable.

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,8 +11,15 @@
  * For more information, visit <http://getsproutcms.com>.
  */
 
-use Sprout\App;
+use Sprout\Core\BaseApp;
+use Sprout\Helpers\Config;
+
+$app = Config::get('config.app');
+
+if (!is_subclass_of($app, BaseApp::class)) {
+    throw new RuntimeException("Application must extend " . BaseApp::class);
+}
 
 // Boot up and go.
-$app = App::instance();
-$app->run();
+$instance = $app::instance();
+$instance->run();
