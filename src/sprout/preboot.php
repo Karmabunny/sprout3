9b2a2e117b2fef82257c75bd8ee8fb07afc12fc4
Gwilyn <gwilyn@karmabunny.com.au>
2026-06-15 16:43:58 +1000

    Move preboot + kohana shims.

--- src/sprout/preboot.php
+++ src/sprout/preboot.php
@@ -1,24 +1,2 @@
 <?php
-/**
- * Good spot for namespace aliases.
- *
- * This is loaded on every request - caution please.
- */
-
-class_alias(\karmabunny\pdb\Exceptions\QueryException::class, \Sprout\Exceptions\QueryException::class);
-class_alias(\karmabunny\pdb\Exceptions\RowMissingException::class, \Sprout\Exceptions\RowMissingException::class);
-class_alias(\karmabunny\pdb\Exceptions\ConstraintQueryException::class, \Sprout\Exceptions\ConstraintQueryException::class);
-class_alias(\karmabunny\pdb\Exceptions\TransactionRecursionException::class, \Sprout\Exceptions\TransactionRecursionException::class);
-class_alias(\karmabunny\interfaces\HttpExceptionInterface::class, \Sprout\Exceptions\HttpExceptionInterface::class);
-
-class_alias(\karmabunny\kb\ValidationException::class, \Sprout\Exceptions\ValidationException::class);
-
-// Old error constants.
-define('E_KOHANA', 42);
-define('E_PAGE_NOT_FOUND', 43);
-define('E_DATABASE_ERROR', 44);
-
-// Load core files
-require __DIR__ . '/core/utf8.php';
-require __DIR__ . '/core/Event.php';
-require __DIR__ . '/core/Kohana.php';
+require_once dirname(__DIR__) . '/preboot.php';
